952822cbf0fbee6b1b039570ce501a261eab7302
Ich <Ich@Win7.fritz.box>
2020-04-15 11:56:15 +0200

    - finished implementing get() function of data providers

--- src/main/java/org/hkijena/mcat/api/dataproviders/ClusterCentersFromFileProvider.java
+++ src/main/java/org/hkijena/mcat/api/dataproviders/ClusterCentersFromFileProvider.java
@@ -1,5 +1,17 @@
 package org.hkijena.mcat.api.dataproviders;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.apache.commons.math3.ml.clustering.CentroidCluster;
+import org.apache.commons.math3.ml.clustering.DoublePoint;
+import org.hkijena.mcat.api.MCATCentroidCluster;
 import org.hkijena.mcat.api.datatypes.ClusterCentersData;
 
 /**
@@ -17,8 +29,27 @@ public class ClusterCentersFromFileProvider extends FileDataProvider<ClusterCent
 
     @Override
     public ClusterCentersData get() {
-    	//TODO
-    	return null;
+    	List<MCATCentroidCluster<DoublePoint>> centroids = new ArrayList<MCATCentroidCluster<DoublePoint>>();
+
+    	try {
+    		String filePath = getFilePath().toString();
+        	
+        	BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
+        	String line = null;
+        	
+        	while((line = br.readLine()) != null) {
+        		double[] arr = Stream.of(line.split(","))
+                        .mapToDouble (Double::parseDouble)
+                        .toArray();
+        		centroids.add(new MCATCentroidCluster<DoublePoint>(new DoublePoint(arr)));
+        	}
+        	
+        	br.close();
+        	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	return new ClusterCentersData(centroids);
     }
 
     @Override
