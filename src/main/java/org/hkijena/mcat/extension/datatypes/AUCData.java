efd96af5e709f50132f30d20b62b273fb28f2018
imaris <imaris@10.96.10.137>
2020-08-19 10:29:46 +0200

    - added copyright header to source files

--- src/main/java/org/hkijena/mcat/extension/datatypes/AUCData.java
+++ src/main/java/org/hkijena/mcat/extension/datatypes/AUCData.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright by Bianca Hoffmann, Ruman Gerst, Zoltán Cseresnyés and Marc Thilo Figge
+ *
+ * Research Group Applied Systems Biology
+ * Leibniz Institute for Natural Product Research and Infection Biology - Hans Knöll Institute (HKI)
+ * Beutenbergstr. 11a, 07745 Jena, Germany
+ *
+ * https://www.leibniz-hki.de/en/applied-systems-biology.html
+ *
+ * The project code is licensed under BSD 2-Clause.
+ * See the LICENSE file provided with the code for the full license.
+ *
+ *******************************************************************************/
 package org.hkijena.mcat.extension.datatypes;
 
 import ij.measure.ResultsTable;
--- src/main/java/org/hkijena/mcat/utils/StringUtils.java
+++ src/main/java/org/hkijena/mcat/utils/StringUtils.java
@@ -1,14 +1,16 @@
-/*
- * Copyright by Ruman Gerst
- * Research Group Applied Systems Biology - Head: Prof. Dr. Marc Thilo Figge
- * https://www.leibniz-hki.de/en/applied-systems-biology.html
- * HKI-Center for Systems Biology of Infection
- * Leibniz Institute for Natural Product Research and Infection Biology - Hans Knöll Insitute (HKI)
- * Adolf-Reichwein-Straße 23, 07745 Jena, Germany
+/*******************************************************************************
+ * Copyright by Bianca Hoffmann, Ruman Gerst, Zoltán Cseresnyés and Marc Thilo Figge
  *
- * This code is licensed under BSD 2-Clause
- * See the LICENSE file provided with this code for the full license.
- */
+ * Research Group Applied Systems Biology
+ * Leibniz Institute for Natural Product Research and Infection Biology - Hans Knöll Institute (HKI)
+ * Beutenbergstr. 11a, 07745 Jena, Germany
+ *
+ * https://www.leibniz-hki.de/en/applied-systems-biology.html
+ *
+ * The project code is licensed under BSD 2-Clause.
+ * See the LICENSE file provided with the code for the full license.
+ *
+ *******************************************************************************/
 
 package org.hkijena.mcat.utils;
 
