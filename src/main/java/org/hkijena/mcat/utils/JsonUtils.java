b0d930a7597895c6e61ea86f3df63e273e9e96ee
Ich <Ich@Win7>
2020-12-16 11:26:44 +0100

    - updated Copyright header

--- src/main/java/org/hkijena/mcat/utils/JsonUtils.java
+++ src/main/java/org/hkijena/mcat/utils/JsonUtils.java
@@ -1,16 +1,15 @@
 /*******************************************************************************
- * Copyright by Bianca Hoffmann, Ruman Gerst, Zoltán Cseresnyés and Marc Thilo Figge
- *
- * Research Group Applied Systems Biology
- * Leibniz Institute for Natural Product Research and Infection Biology - Hans Knöll Institute (HKI)
- * Beutenbergstr. 11a, 07745 Jena, Germany
- *
+ * Copyright by Dr. Bianca Hoffmann, Ruman Gerst, Dr. Zoltán Cseresnyés and Prof. Dr. Marc Thilo Figge
+ * 
+ * Research Group Applied Systems Biology - Head: Prof. Dr. Marc Thilo Figge
  * https://www.leibniz-hki.de/en/applied-systems-biology.html
- *
+ * HKI-Center for Systems Biology of Infection
+ * Leibniz Institute for Natural Product Research and Infection Biology - Hans Knöll Insitute (HKI)
+ * Adolf-Reichwein-Straße 23, 07745 Jena, Germany
+ * 
  * The project code is licensed under BSD 2-Clause.
  * See the LICENSE file provided with the code for the full license.
- *
- *******************************************************************************/
+ ******************************************************************************/
 package org.hkijena.mcat.utils;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
